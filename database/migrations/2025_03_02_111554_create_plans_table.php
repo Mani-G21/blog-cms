e08b4d046c0563e6d58d6b684689101ac6f17eb3
Mani-G21 <ghindwani.manish.st@gmail.com>
2025-03-03 14:33:50 +0530

    Refactor: - Instead of making a seperate file for creating column stripe_price_id in plans table, the plans table itself now has stripe_price_id column on migration. - Adjust the sidebar contents on the admin side. - Created new functions in RoutingHelper.

--- database/migrations/2025_03_02_111554_create_plans_table.php
+++ database/migrations/2025_03_02_111554_create_plans_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('stripe_plan_id');
+            $table->string('stripe_price_id');
             $table->string('interval');
             $table->unsignedInteger('price');
             $table->unsignedInteger('articles_per_month');
@@ -24,10 +25,10 @@ return new class extends Migration
         });
 
         $plans = collect([
-            ['name' => 'Basic Monthly Plan', 'stripe_plan_id' => 'prod_Rru9YR8fhFUXJZ', 'interval' => 'monthly', 'price' => 10, 'articles_per_month' => 100],
-            ['name' => 'Pro Monthly Plan', 'stripe_plan_id' => 'prod_RruA6IWkCJ80lv', 'interval' => 'monthly', 'price' => 29, 'articles_per_month' => 500],
-            ['name' => 'Basic Yearly Plan (10% off)', 'stripe_plan_id' => 'prod_RruBd7ilvHtwKx', 'interval' => 'yearly', 'price' => 108, 'articles_per_month' => 1200],
-            ['name' => 'Pro Yearly Plan (10% off)', 'stripe_plan_id' => 'prod_RruCTI7QAuA2GB', 'interval' => 'yearly', 'price' => 313, 'articles_per_month' => 6000]
+            ['name' => 'Basic Monthly Plan', 'stripe_plan_id' => 'prod_Rru9YR8fhFUXJZ', 'stripe_price_id' => 'price_1QyALYJg2PxnO5v3mZ1ItCFu','interval' => 'monthly', 'price' => 10, 'articles_per_month' => 100],
+            ['name' => 'Pro Monthly Plan', 'stripe_plan_id' => 'prod_RruA6IWkCJ80lv', 'stripe_price_id' => 'price_1QyAMOJg2PxnO5v3inhrbKpr', 'interval' => 'monthly', 'price' => 29, 'articles_per_month' => 500],
+            ['name' => 'Basic Yearly Plan (10% off)', 'stripe_plan_id' => 'prod_RruBd7ilvHtwKx','stripe_price_id' => 'price_1QyANEJg2PxnO5v3Hr5yiPpd', 'interval' => 'yearly', 'price' => 108, 'articles_per_month' => 1200],
+            ['name' => 'Pro Yearly Plan (10% off)', 'stripe_plan_id' => 'prod_RruCTI7QAuA2GB', 'stripe_price_id' => 'price_1QyAOOJg2PxnO5v3mJuQpbgc','interval' => 'yearly', 'price' => 313, 'articles_per_month' => 6000]
         ]);
 
         $plans->each(function($plan){
--- resources/views/admin/layouts/_sidebar.blade.php
+++ resources/views/admin/layouts/_sidebar.blade.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href={{route('admin.dashboard')}}>
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href={{ route('admin.dashboard') }}>
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
@@ -30,81 +30,72 @@
 
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item {{ \App\Helpers\RoutingHelper::isCategoryRoute() ? 'active' : '' }}">
-        <a class="nav-link {{ \App\Helpers\RoutingHelper::isCategoryRoute() ? '' : 'collapsed' }}" href="#" data-toggle="collapse" data-target="#collapseCategory"
-           aria-expanded="true" aria-controls="collapseCategory">
+        <a class="nav-link {{ \App\Helpers\RoutingHelper::isCategoryRoute() ? '' : 'collapsed' }}" href="#"
+            data-toggle="collapse" data-target="#collapseCategory" aria-expanded="true"
+            aria-controls="collapseCategory">
             <i class="fas fa-fw fa-cog"></i>
             <span>Category</span>
         </a>
-        <div id="collapseCategory" class="collapse {{ \App\Helpers\RoutingHelper::isCategoryRoute() ? 'show' : '' }}" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div id="collapseCategory" class="collapse {{ \App\Helpers\RoutingHelper::isCategoryRoute() ? 'show' : '' }}"
+            aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Category Operations:</h6>
-                <a class="collapse-item {{ \App\Helpers\RoutingHelper::isCategoryIndex() ? 'active' : '' }}" href="{{ route('admin.categories.index') }}">Show All</a>
-                <a class="collapse-item {{ \App\Helpers\RoutingHelper::isCategoryCreate() ? 'active' : '' }}" href="{{ route('admin.categories.create') }}">Create Category</a>
+                <a class="collapse-item {{ \App\Helpers\RoutingHelper::isCategoryIndex() ? 'active' : '' }}"
+                    href="{{ route('admin.categories.index') }}">Show All</a>
+                <a class="collapse-item {{ \App\Helpers\RoutingHelper::isCategoryCreate() ? 'active' : '' }}"
+                    href="{{ route('admin.categories.create') }}">Create Category</a>
 
             </div>
         </div>
     </li>
 
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-            aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Utilities</span>
+    <li class="nav-item {{ \App\Helpers\RoutingHelper::isTagRoute() ? 'active' : '' }}">
+        <a class="nav-link {{ \App\Helpers\RoutingHelper::isTagRoute() ? '' : 'collapsed' }}" href="#"
+            data-toggle="collapse" data-target="#collapseTag" aria-expanded="true" aria-controls="collapseTag">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Tag</span>
         </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+        <div id="collapseTag" class="collapse {{ \App\Helpers\RoutingHelper::isTagRoute() ? 'show' : '' }}"
+            aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="utilities-color.html">Colors</a>
-                <a class="collapse-item" href="utilities-border.html">Borders</a>
-                <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                <a class="collapse-item" href="utilities-other.html">Other</a>
+                <h6 class="collapse-header">Tag Operations:</h6>
+                <a class="collapse-item {{ \App\Helpers\RoutingHelper::isTagIndex() ? 'active' : '' }}"
+                    href="{{ route('admin.tags.index') }}">Show All</a>
+                <a class="collapse-item {{ \App\Helpers\RoutingHelper::isTagCreate() ? 'active' : '' }}"
+                    href="{{ route('admin.tags.create') }}">Create Tag</a>
+
             </div>
         </div>
     </li>
+
+
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item {{ \App\Helpers\RoutingHelper::isPostRoute() ? 'active' : '' }}">
+        <a class="nav-link {{ \App\Helpers\RoutingHelper::isPostRoute() ? '' : 'collapsed' }}" href="#"
+            data-toggle="collapse" data-target="#collapsePost" aria-expanded="true" aria-controls="collapsePost">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Post</span>
+        </a>
+
+        </a>
+        <div id="collapsePost" class="collapse {{ \App\Helpers\RoutingHelper::isPostRoute() ? 'show' : '' }}"
+            aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+
+                <h6 class="collapse-header">Post Operations:</h6>
+                <a class="collapse-item {{ \App\Helpers\RoutingHelper::isPostIndex() ? 'active' : '' }}"
+                    href="{{ route('admin.posts.index') }}">Show All</a>
+                <a class="collapse-item {{ \App\Helpers\RoutingHelper::isPostCreate() ? 'active' : '' }}"
+                    href="{{ route('admin.posts.create') }}">Create Post</a>
+
+            </div>
+        </div>
+    </li>
+
 
     <!-- Divider -->
     <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Addons
-    </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-            aria-expanded="true" aria-controls="collapsePages">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-        </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
-                <a class="collapse-item" href="login.html">Login</a>
-                <a class="collapse-item" href="register.html">Register</a>
-                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Other Pages:</h6>
-                <a class="collapse-item" href="404.html">404 Page</a>
-                <a class="collapse-item" href="blank.html">Blank Page</a>
-            </div>
-        </div>
-    </li>
-
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span></a>
-    </li>
-
-    <!-- Nav Item - Tables -->
-    <li class="nav-item">
-        <a class="nav-link" href="tables.html">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Tables</span></a>
-    </li>
 
     <!-- Divider -->
     <hr class="sidebar-divider d-none d-md-block">
