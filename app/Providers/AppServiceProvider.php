7915cae0fad441d9b9a05f01d4dce383ebabb87c
Mani-G21 <ghindwani.manish.st@gmail.com>
2025-03-05 14:44:14 +0530

    Chore: - Registered the CategoryPolicy and TagPolicy in the AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,13 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use App\Observers\PostObserver;
+use App\Policies\CategoryPolicy;
 use App\Policies\PostPolicy;
+use App\Policies\TagPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -27,5 +31,7 @@ class AppServiceProvider extends ServiceProvider
 
         //Register the policies
         Gate::policy(Post::class, PostPolicy::class);
+        Gate::policy(Category::class, CategoryPolicy::class);
+        Gate::policy(Tag::class, TagPolicy::class);
     }
 }
