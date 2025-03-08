d87898df2384633c561fd773db3e779fc848f2e1
Mani-G21 <ghindwani.manish.st@gmail.com>
2025-03-08 16:29:33 +0530

    Chore: - Added column slug in tags table. - Added slug to fillable and created a mutator in Tag.php

---
+++ database/migrations/2025_03_08_105239_add_column_slug_to_tags_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Tag;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tags', function (Blueprint $table) {
+            $table->string('slug')->after('name')->nullable();
+        });
+
+        Tag::all()->each(function($tag){
+            $tag->slug = Str::slug($tag->name);
+            $tag->save();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tags', function (Blueprint $table) {
+            //
+        });
+    }
+};
